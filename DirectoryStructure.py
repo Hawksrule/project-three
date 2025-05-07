06f41237bdf7e9df6e9551f960f5e27390692087
Hawksrule <jeremycline8833@gmail.com>
2025-05-07 11:33:07 -0500

    added nice I/O and folder size calculator

--- DirectoryStructure.py
+++ DirectoryStructure.py
@@ -1,3 +1,5 @@
+import time
+
 class DirectoryNode:
     def __init__(self, name, size = 0, is_leaf = False):
         self.name = name
@@ -37,6 +39,24 @@ class Directory:
                     if temp:
                         return temp
     
+    def find_size(self, root, folder_name = None):
+        if root:
+            if root.name == folder_name:
+                return self.add_sizes(root)
+            elif not root.is_leaf:
+                for child in root.children:
+                    temp = self.find_size(child, folder_name)
+                    if temp:
+                        return temp
+    
+    def add_sizes(self, root, size = 0):
+        if not root.is_leaf:
+            for child in root.children:
+                size += self.add_sizes(child)
+        else:
+            size = root.size
+        return size
+    
     def traverse_preorder(self, node):
         if node:
             print(node.name, node.size, end=" ")
@@ -60,5 +80,29 @@ if __name__ == "__main__":
     files.insert_child(root, "Root/Folder2/SubFolder1/SubFolder2", DirectoryNode(name="File4.txt", size=15, is_leaf=True))
     files.insert_child(root, "Root/Folder2", DirectoryNode(name="File5.pdf", size=30, is_leaf=True))
     
+    print("Welcome to the Root Directory!",
+          "\nYou can select from a handful",
+          "\nof queries to withdraw data and",
+          "\nstatistics from the directory.")
     
-    files.find_file(root, "File4.txt")
+    query = input("Please select: Find file: 'FileName', Folder size: 'FolderName', Shortest Path: 'FileName'\nFind all: 'Type' (Folder, File, .txt, .pdf), Find files: '(>, <, ==)' 'Size': ").split(':')
+    
+    while len(query) < 2:
+        query = input("You must provide a query formatted as such: 'Find file: File3.txt': ")
+        
+    query = [x.strip() for x in query]
+        
+    if query[0].lower() == "find file":
+        print(f"Finding {query[1]} in Root...")
+        start_time = time.time()
+        print(f"Path: {files.find_file(root, query[1])}")
+        end_time = time.time()
+        print(f"Elapsed time: {end_time - start_time}")
+        
+    elif query[0].lower() == "folder size":
+        print(f"Calculating {query[1]} size...")
+        start_time = time.time()
+        print(f"Total size: {files.find_size(root, query[1])}mb")
+        end_time = time.time()
+        print(f"Elapsed time: {end_time - start_time}")
+        
